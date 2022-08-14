4da5e94d1963552ba3a94d51d05691b25302db6d
Deepika Juneja <deepsjunejadj@gmail.com>
2022-08-14 12:57:11 +0530

    ToDo App using React

---
+++ src/components/Todo.js
@@ -0,0 +1,119 @@
+import React, {useState, useRef, useEffect} from "react";
+
+
+function usePreviousState(state) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = state;
+  });
+
+  return ref.current;
+}
+
+
+export default function Todo(task) {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePreviousState(isEditing);
+
+  const editTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={task.id}>
+          New name for {task.name}
+        </label>
+        <input
+          id={task.id}
+          className="todo-text"
+          type="text"
+          value={newName}
+          onChange={handleChange}
+          ref={editFieldRef}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+          <span className="visually-hidden">renaming {task.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {task.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+        <input
+          id={task.id}
+          type="checkbox"
+          defaultChecked={task.done}
+          onChange={() => task.toggleCompletedTask(task.id)}
+        />
+        <label className="todo-label" htmlFor={task.id}>
+          {task.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+        >
+          Edit <span className="visually-hidden">{task.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => task.deleteTask(task.id)}
+        >
+          Delete <span className="visually-hidden">{task.name}</span>
+        </button>
+      </div>
+    </div>
+  );
+
+
+  function handleChange(event) {
+    setNewName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if(newName !== "")
+      task.editTask(task.id, newName);
+    else
+      alert("Cannot add an empty task!!!");
+
+    setNewName("");
+    setEditing(false);
+  }
+
+
+  useEffect(() => {
+
+    if(!wasEditing && isEditing)
+      editFieldRef.current.focus();
+    
+    if(wasEditing && !isEditing)
+      editButtonRef.current.focus();
+
+  }, [wasEditing, isEditing]);
+  
+  return <li className="todo">{isEditing ? editTemplate : viewTemplate}</li>;
+
+}
+  
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+
+const TASKS = [
+  { name : "Eat", done : false, id : "task-0" },
+  { name : "Sleep", done : false, id : "task-1" },
+  { name : "Code", done : false, id : "task-2" },
+  { name : "Repeat", done : true, id : "task-3" }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks = {TASKS} />
   </React.StrictMode>
 );
 
